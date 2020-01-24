40c882a2eb89ee9b57948b76067ff00a7bf0a785
KoukiOokawa <32385663+wc-okawa@users.noreply.github.com>
2020-01-24 14:10:51 +0900

    セミコロンはいらん

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import VueCompostionApi from "@vue/composition-api"
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+Vue.use(VueCompostionApi)
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount("#app")
