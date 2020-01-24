c51af1d4fe09515307d7ff6c2d1ff5fe9890b6f4
KoukiOokawa <32385663+wc-okawa@users.noreply.github.com>
2020-01-24 15:51:27 +0900

    propsできた

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="app">
     {{state.test}}
+    {{refStr}}
+    <Test message="てすとまる"></Test>
     <router-view />
   </div>
 </template>
 
 <script lang="ts">
-import { createComponent, reactive } from '@vue/composition-api'
+import { createComponent, reactive, ref } from '@vue/composition-api'
+import Test from './components/Test.vue'
 export default createComponent ({
+  components: {
+    Test
+  },
   setup() {
     const state = reactive<{
       test: string
     }>({
       test: "ほぎぃ"
     })
+    const refStr = ref<string>("れふれふ")
 
     return {
-      state
+      state,
+      refStr
     }
   }
 })
---
+++ src/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="test">
+    コンポーネントのテスト
+    {{hoge}}
+  </div>
+</template>
+<script lang="ts">
+type Props = {
+  message: String
+}
+import { createComponent, reactive, ref } from '@vue/composition-api'
+export default createComponent({
+  props: {
+    message: {
+      type: String,
+      default: "default messageですよ"
+    }
+  },
+  setup(props: Props) {
+    const hoge = ref<string>(props.message)
+
+    return {
+      hoge
+    }
+  }
+})
+</script>
